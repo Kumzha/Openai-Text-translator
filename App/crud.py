2fdc26666c5f57e9fd7e988d238933866c3b1c9e
kumzha <kumza.dominykas@gmail.com>
2024-07-11 16:28:50 +0300

    added openapi request for translation, connection to db, and a model for database storage

--- App/crud.py
+++ App/crud.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Session
+from sqlalchemy.orm import Session
 import models
+
 def create_translation_task(db:Session, text:str, languages: list):
     task = models.TranslationTask(text=text, languages=languages)
 
@@ -12,5 +13,11 @@ def create_translation_task(db:Session, text:str, languages: list):
 def get_translation_task(db: Session, task_id: int):
     return db.query(models.TranslationTask).filter(models.TranslationTask.id == task_id).first()
 
-# def update_translation_task(db: Session, task_id: int, translations: dict):
-    
+def update_translation_task(db: Session, task_id: int, translations: dict):
+    task = db.query(models.TranslationTask).filter(models.TranslationTask.id == task_id).first()
+    task.translations = translations
+    task.status = "completed"
+
+    db.commit()
+    db.refresh(task)
+    return task
--- App/main.py
+++ App/main.py
@@ -1,12 +1,34 @@
 from fastapi import FastAPI, BackgroundTasks, HTTPException, Request, Depends
 from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.orm import Session
 import schemas
-
+import crud
+import models
+from databse import get_db, engine
 from fastapi.templating import Jinja2Templates
+from utily import perform_translation
 
 app = FastAPI()
+
+#Learn
+models.Base.metadata.create_all(bind=engine)
+
+#Learn
 templates = Jinja2Templates(directory="templates")
+
+app.app_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credential=True,
+    allow_methods=["*"],
+    allow_header=["*"],
+)
+
+
+
+######################################################################################
+# Endpoints
 
 @app.get('/index', response_class=HTMLResponse)
 def index(request: Request):
@@ -21,19 +43,12 @@ def index(request: Request):
 
 
 
-#Learn
-# app.app_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],
-#     allow_credential=True,
-#     allow_methods=["*"],
-#     allow_header=["*"],
-# )
-
 #Learn endpoints
-# @app.post("/translate", response_model=schemas.TaskResponse)
-# def translate(request: schemas.TranslationRequest):
-#     task = crud.create_translation_task(x,y, , p)
-#     backgournd_tasks.add+task(perform_translation, task.id, request.text, request.languages, db)
 
-#     return {"task_id": {task.id}}
+@app.post("/translate", response_model=schemas.TaskResponse)
+def translate(request: schemas.TranslationRequest, background_tasks: BackgroundTasks, db: Session):
+    task = crud.create_translation_task(get_db.db, request.text, request.languages, BackgroundTasks)
+
+    background_tasks.add+task(perform_translation, task.id, request.text, request.languages, db)
+
+    return {"task_id": {task.id}}
